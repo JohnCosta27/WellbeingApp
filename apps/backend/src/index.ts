eb96995c4e3a2b0c90f6ba44fd9f7555b12944d0
John Costa <johncosta027@gmail.com>
2023-03-19 16:44:39 +0000

    FEAT: Adding authorization to graphql

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,21 +3,30 @@ import path from "path";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import { MentalEnergy } from "@wellbeing/graphql-types";
+import { MentalEnergy, Resolvers } from "@wellbeing/graphql-types";
 import express from "express";
 
 // @ts-ignore
 import cors from "cors";
 
 // @ts-ignore
-import { json } from 'body-parser';
+import { json } from "body-parser";
 import AuthRouter from "./auth";
+import { verifyJwt } from "./util/jwt";
+import { GraphQLError } from "graphql";
 
-const file = fs.readFileSync(path.join(__dirname, "../../../packages/graphql/schema.graphql"), "utf8");
+const file = fs.readFileSync(
+  path.join(__dirname, "../../../packages/graphql/schema.graphql"),
+  "utf8"
+);
 
-const resolvers = {
+interface Context {
+  uuid: string;
+}
+
+const resolvers: Resolvers<Context> = {
   Query: {
-    async MentalEnergy(): Promise<MentalEnergy> {
+    async MentalEnergy(_parent, _args, context): Promise<MentalEnergy> {
       return {
         date: 50,
         level: 123,
@@ -26,7 +35,7 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
+const server = new ApolloServer<Context>({
   typeDefs: `#graphql
     ${file}
 `,
@@ -39,16 +48,47 @@ app.use(json());
 app.use(cors());
 
 app.use((_, res, next) => {
-  res.contentType('application/json');
+  res.contentType("application/json");
   next();
-})
+});
 
-app.use('/auth', AuthRouter);
+app.use("/auth", AuthRouter);
 
 (async () => {
   await server.start();
-  app.use("/graphql", expressMiddleware(server));
+  app.use(
+    "/graphql",
+    expressMiddleware(server as any, {
+      context: async ({ req }) => {
+
+        if (!req.headers.authorization) {
+          throw GetAuthorizedError();
+        }
+        const token = verifyJwt(req.headers.authorization);
+
+        const timeNow = new Date().getTime() / 1000;
+        if (!token || token.exp < timeNow) {
+          throw GetAuthorizedError();
+        }
+
+        return {
+          uuid: token.uuid,
+        };
+      },
+    })
+  );
   app.listen(3030, () => {
     console.log("Server running");
   });
 })();
+
+function GetAuthorizedError(): GraphQLError {
+  return new GraphQLError('User is authorizated', {
+    extensions: {
+      code: 'UNAUTHORIZED',
+      http: {
+        status: 401,
+      }
+    }
+  })
+}
