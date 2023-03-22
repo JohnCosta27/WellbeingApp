ac1767f0dc1b091d417548a21fa55105b1da6256
John Costa <johncosta027@gmail.com>
2023-03-22 00:46:12 +0000

    FEAT: Adding register pages

--- apps/frontend/src/Auth/Login.tsx
+++ apps/frontend/src/Auth/Login.tsx
@@ -1,5 +1,5 @@
 import { FC, SyntheticEvent, useCallback, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { setToken } from ".";
 import { AuthRequests } from "../Network";
 
@@ -9,40 +9,107 @@ export const Login: FC = () => {
 
   const nav = useNavigate();
 
-  const onLogin = useCallback(async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const res = await AuthRequests.login({
-      email,
-      password,
-    });
-    if (res.data.type === 'error') {
-      //TODO: Handle this
-      return;
-    }
+  const onLogin = useCallback(
+    async (e: SyntheticEvent) => {
+      e.preventDefault();
+      const res = await AuthRequests.login({
+        email,
+        password,
+      });
+      if (res.data.type === "error") {
+        //TODO: Handle this
+        return;
+      }
 
-    setToken('access', res.data.body.access);
-    setToken('refresh', res.data.body.refresh);
+      setToken("access", res.data.body.access);
+      setToken("refresh", res.data.body.refresh);
 
-    nav('/');
-  }, [email, password, nav]);
+      nav("/");
+    },
+    [email, password, nav]
+  );
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <input
-        type="email"
-        className="input"
-        placeholder="Email..."
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        className="input"
-        placeholder="Password..."
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" className="btn" onClick={onLogin}>
-        Send Email
-      </button>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-base-300">
+      <div className="py-16 px-8 max-w-5xl flex flex-col gap-4 bg-neutral rounded-xl">
+        <input
+          type="email"
+          className="input"
+          placeholder="Email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="Password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className="btn btn-primary" onClick={onLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const Register: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const nav = useNavigate();
+
+  const onLogin = useCallback(
+    async (e: SyntheticEvent) => {
+      e.preventDefault();
+      if (!(confirmPassword === password)) {
+        alert("Passwords don't match");
+        return;
+      }
+
+      const res = await AuthRequests.register({
+        email,
+        password,
+      });
+
+      if (res.data.type === "error") {
+        //TODO: Handle this
+        return;
+      }
+
+      setToken("access", res.data.body.access);
+      setToken("refresh", res.data.body.refresh);
+
+      nav("/");
+    },
+    [email, password, nav]
+  );
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-base-300">
+      <div className="py-16 px-8 max-w-5xl flex flex-col gap-4 bg-neutral rounded-xl">
+        <input
+          type="email"
+          className="input"
+          placeholder="Email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="Password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="Confirm Password..."
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit" className="btn btn-primary" onClick={onLogin}>
+          Register
+        </button>
+      </div>
     </div>
   );
 };
--- apps/frontend/src/Router.tsx
+++ apps/frontend/src/Router.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { ProtectedRoute, Login } from "./Auth";
+import { ProtectedRoute, Login, Register } from "./Auth";
 
 export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
   {
     path: "/",
