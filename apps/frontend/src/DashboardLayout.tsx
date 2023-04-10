1ad9dff7583877918c1d45605a98be1bec6526b6
John Costa <johncosta027@gmail.com>
2023-04-10 14:16:11 +0100

    FIX: Minor styles

--- apps/frontend/src/DashboardLayout.tsx
+++ apps/frontend/src/DashboardLayout.tsx
@@ -10,7 +10,7 @@ export const DashboardLayout: FC = () => {
         <TopbarItem onNav="/how">Who</TopbarItem>
         <TopbarItem onNav="/how">IBrand</TopbarItem>
       </div>
-      <div className="w-full h-full p-6">
+      <div className="w-full h-full p-6 overflow-y-scroll">
         <Outlet />
       </div>
     </div>
