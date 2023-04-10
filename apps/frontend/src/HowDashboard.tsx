e4d174ecb71d77bdea1b1d5b8ed03dcd92d9025b
John Costa <johncosta027@gmail.com>
2023-04-10 18:54:35 +0100

    STYLES: Dashboard layout

--- apps/frontend/src/HowDashboard.tsx
+++ apps/frontend/src/HowDashboard.tsx
@@ -78,7 +78,12 @@ export const HowDashboard: FC = () => {
           feeling
         </h4>
       </div>
-      <div className="w-full grid grid-cols-3 gap-x-4 gap-y-6">
+      <div
+        className="w-full grid grid-cols-3 gap-x-4 gap-y-6"
+        style={{
+          gridTemplateRows: "repeat(3, 300px)",
+        }}
+      >
         <Card title="Mental Energy">
           <UIMentalEnergy
             loading={loading}
@@ -93,7 +98,8 @@ export const HowDashboard: FC = () => {
             }
           />
         </Card>
-        <Card title="Wellness Check" className="row-span-2">
+        <Card title="Place holder" />
+        <Card title="Wellness Check" className="row-span-3">
           <WellnessCheck
             lastWords={lastWords.map((w) => w.phrase.phrase)}
             availableWords={
@@ -110,99 +116,37 @@ export const HowDashboard: FC = () => {
             }}
           />
         </Card>
-        <Card />
-        <Card />
-        <Card />
-      </div>
-      <div className="w-full flex bg-white rounded-xl shadow-xl p-4">
-        <div className="w-full flex flex-col">
-          <div className="w-full flex flex-col gap-2">
-            <p className="text-3xl">Last 7 Days</p>
-            <Battery energy={energyAverage} />
-            <p className="text-2xl text-info-content">
-              {data && getMessage(energyAverage)}
-            </p>
+        <Card title="Energy Level Graph" className="row-span-2 col-span-2">
+          <div className="w-full h-full">
+            {data && (
+              <Line
+                data={{
+                  labels: sortedEnergy.map((m) =>
+                    new Date(m.date).toLocaleString()
+                  ),
+                  datasets: [
+                    {
+                      label: "Your energy",
+                      data: sortedEnergy.map((m) => m.level).slice(0, 10),
+                    },
+                  ],
+                }}
+                options={{
+                  maintainAspectRatio: false,
+                  elements: {
+                    line: {
+                      tension: 0.3,
+                    },
+                  },
+                }}
+              />
+            )}
           </div>
-        </div>
-      </div>
-      <div className="w-full flex bg-white rounded-xl shadow-sm p-4"></div>
-      <div className="w-full flex bg-white rounded-xl shadow-sm p-4">
-        <div className="w-full h-[40vh]">
-          {data && (
-            <Line
-              data={{
-                labels: sortedEnergy.map((m) =>
-                  new Date(m.date).toLocaleString()
-                ),
-                datasets: [
-                  {
-                    label: "Your energy",
-                    data: sortedEnergy.map((m) => m.level).slice(0, 10),
-                  },
-                ],
-              }}
-              options={{
-                maintainAspectRatio: false,
-                elements: {
-                  line: {
-                    tension: 0.3,
-                  },
-                },
-              }}
-            />
-          )}
-        </div>
+        </Card>
       </div>
     </div>
   );
 };
-
-interface BatteryProps {
-  energy: number;
-}
-
-export const Battery: FC<BatteryProps> = ({ energy }) => (
-  <div
-    className="rounded shadow-md w-full h-2"
-    style={{
-      backgroundColor: getPastelColor(energy * 100, 0.2),
-    }}
-  >
-    <div
-      className="h-full"
-      style={{
-        backgroundColor: getPastelColor(energy * 100, 1),
-        width: `${energy * 100}%`,
-      }}
-    />
-  </div>
-);
-
-function getPastelColor(percent: number, transparancy: number) {
-  const formatPercent = Math.min(100, Math.max(0, percent));
-
-  const red = Math.round(202 - (75 * formatPercent) / 100);
-  const green = Math.round(127 + (99 * formatPercent) / 100);
-  const blue = Math.round(127 - (25 * formatPercent) / 100);
-
-  return `rgba(${red}, ${green}, ${blue}, ${transparancy})`;
-}
-
-function getMessage(percent: number): string {
-  if (percent > 0.75) {
-    return "Looks like you are doing great!";
-  }
-
-  if (percent > 0.5) {
-    return "Doing Ok, keep going!";
-  }
-
-  if (percent > 0.25) {
-    return "Not your best days, but we can help if you need!";
-  }
-
-  return "Please let us help, see these links";
-}
 
 function getLast7DaysEnergy(energies: MentalEnergy[]): number {
   return energies.reduce((p, n) => p + n.level, 0) / energies.length;
