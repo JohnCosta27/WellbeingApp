d2a5c97ec674201ab80ad66dd5b9ff27ffd78532
John Costa <johncosta027@gmail.com>
2023-04-17 21:19:13 +0100

    FEAT: Sidebar for mobile

--- apps/frontend/src/DashboardLayout.tsx
+++ apps/frontend/src/DashboardLayout.tsx
@@ -1,19 +1,56 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 import { Outlet } from "react-router";
 import { Link } from "react-router-dom";
 
-export const DashboardLayout: FC = () => (
-  <div className="w-full h-screen bg-base-100 flex">
-    <div className="w-64 flex-col bg-white px-2 py-8 shadow-xl border-r-2 hidden md:flex">
-      <TopbarItem onNav="/how">How</TopbarItem>
-      <TopbarItem onNav="/who">Who</TopbarItem>
-      <TopbarItem onNav="/how">What</TopbarItem>
-    </div>
-    <div className="w-full h-full p-6 overflow-y-scroll bg-[#F6F8FA]">
-      <Outlet />
-    </div>
-  </div>
+const HamburgerIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </svg>
 );
+
+export const DashboardLayout: FC = () => {
+  const [openSidebar, setOpenSidebar] = useState(true);
+
+  return (
+    <div className="w-full h-screen bg-base-100 flex flex-col overflow-hidden">
+      <div className="w-full min-h-12 bg-secondary-focus shadow-md flex justify-between items-center px-4 text-white">
+        <div className="w-full flex items-center gap-4">
+          <div className="flex md:hidden">
+            <button type="button" onClick={() => setOpenSidebar(!openSidebar)}>
+              <HamburgerIcon />
+            </button>
+          </div>
+          <h1 className="text-xl">Wellbeing App</h1>
+        </div>
+      </div>
+      <div className="w-full h-full flex">
+        {openSidebar && (
+          <div className="w-64 flex-col bg-white px-2 py-8 shadow-xl border-r-2">
+            <TopbarItem onNav="/how">How</TopbarItem>
+            <TopbarItem onNav="/who">Who</TopbarItem>
+            <TopbarItem onNav="/how">What</TopbarItem>
+          </div>
+        )}
+        <div className="w-full h-full overflow-y-auto bg-[#F6F8FA]">
+          <div className="w-full h-full p-6">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 interface TopBarItemProps {
   onNav: string;
