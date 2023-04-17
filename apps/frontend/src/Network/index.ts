b518b1afb2ea9a364913223fc196434bf676e014
John Costa <johncosta027@gmail.com>
2023-04-17 19:47:27 +0100

    CHORE: Using environment variables

--- apps/frontend/src/Network/index.ts
+++ apps/frontend/src/Network/index.ts
@@ -1,19 +1,32 @@
 import axios, { AxiosResponse } from "axios";
-import { Auth } from '@wellbeing/graphql-types';
+import { Auth } from "@wellbeing/graphql-types";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 5000,
 });
 
 export const AuthRequests = {
-  login: async(body: Auth.ReqBody): Promise<AxiosResponse<Auth.Response>> => {
-    return await axiosClient.post<Auth.ReqBody, AxiosResponse<Auth.Response>>('/auth/login', body);
+  login: async (body: Auth.ReqBody): Promise<AxiosResponse<Auth.Response>> => {
+    return await axiosClient.post<Auth.ReqBody, AxiosResponse<Auth.Response>>(
+      "/auth/login",
+      body
+    );
   },
-  register: async(body: Auth.ReqBody): Promise<AxiosResponse<Auth.Response>> => {
-    return await axiosClient.post<Auth.ReqBody, AxiosResponse<Auth.Response>>('/auth/register', body);
+  register: async (
+    body: Auth.ReqBody
+  ): Promise<AxiosResponse<Auth.Response>> => {
+    return await axiosClient.post<Auth.ReqBody, AxiosResponse<Auth.Response>>(
+      "/auth/register",
+      body
+    );
   },
-  refresh: async(body: Auth.RefreshReqBody): Promise<AxiosResponse<Auth.Response>> => {
-    return await axiosClient.post<Auth.RefreshReqBody, AxiosResponse<Auth.Response>>('/auth/refresh', body);
+  refresh: async (
+    body: Auth.RefreshReqBody
+  ): Promise<AxiosResponse<Auth.Response>> => {
+    return await axiosClient.post<
+      Auth.RefreshReqBody,
+      AxiosResponse<Auth.Response>
+    >("/auth/refresh", body);
   },
 } as const;
