416298ffa7406e5048519d857b7bb1cdce00acd6
John Costa <johncosta027@gmail.com>
2023-06-13 14:31:24 +0100

    FEAT: MyProgress page

--- apps/frontend/src/MyProgress.tsx
+++ apps/frontend/src/MyProgress.tsx
@@ -1,21 +1,63 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import {
   namedOperations,
+  useAddAssignmentMutation,
   useAddModuleMutation,
   useCurrentUserQuery,
   useModulesQuery,
 } from "@wellbeing/graphql-types";
-import { FC } from "react";
+import { FC, useCallback, useState } from "react";
 import { Card } from "./ui";
 
 export const MyProgress: FC = () => {
   const user = useCurrentUserQuery();
   const modules = useModulesQuery();
 
-  console.log(user.data?.currentUser.modules);
+  const [selectedModule, setSelectedModule] = useState<string | undefined>(
+    undefined
+  );
+  const [assignmentScore, setAssignmentScore] = useState(0);
+  const [assignmentName, setAssignmentName] = useState<string | undefined>(
+    undefined
+  );
+  const [subError, setSubError] = useState<string | undefined>(undefined);
 
   const [addModule] = useAddModuleMutation({
     refetchQueries: [namedOperations.Query.CurrentUser],
   });
+
+  const [addAssignment] = useAddAssignmentMutation({
+    variables: {
+      moduleId: selectedModule || "",
+      score: assignmentScore,
+      name: assignmentName || "",
+    },
+    refetchQueries: [namedOperations.Query.CurrentUser],
+  });
+
+  const onSubmitAssignment = useCallback(() => {
+    if (assignmentScore < 0 || assignmentScore > 100) {
+      setSubError("Assignment score must be a percentage (0 to 100)");
+      return;
+    }
+
+    if (!selectedModule) {
+      setSubError("You must select a module");
+      return;
+    }
+
+    if (!assignmentName) {
+      setSubError("You must give your assignment a name");
+      return;
+    }
+
+    addAssignment();
+  }, [addAssignment, assignmentName, assignmentScore, selectedModule]);
 
   return (
     <div className="w-full flex flex-col gap-4">
@@ -52,10 +94,64 @@ export const MyProgress: FC = () => {
               ))}
           </div>
         </Card>
-        <Card
-          title="Submit Assignment"
-          className="col-span-1 row-span-3"
-        ></Card>
+        <Card title="Submit Assignment" className="col-span-1 row-span-3">
+          <div className="dropdown" onClick={() => setSubError(undefined)}>
+            <label tabIndex={0} className="btn m-1">
+              {/** lol */}
+              {!selectedModule
+                ? "Select Module"
+                : user.data?.currentUser.modules.find(
+                    (m) => m.module.id === selectedModule
+                  )?.module.name || "Select Module"}
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {user.data?.currentUser.modules.map((m) => (
+                <li
+                  key={m.module.id}
+                  onClick={() => setSelectedModule(m.module.id)}
+                >
+                  <a>{m.module.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <label className="label" htmlFor="score-input">
+            <span className="label-text">Score</span>
+          </label>
+          <input
+            className="input input-bordered input-primary w-full max-w-xs"
+            placeholder="99%"
+            min={0}
+            max={100}
+            type="number"
+            id="score-input"
+            value={assignmentScore}
+            onFocus={() => setSubError(undefined)}
+            onChange={(e) => setAssignmentScore(parseFloat(e.target.value))}
+          />
+          <label className="label" htmlFor="name-input">
+            <span className="label-text">Assignment Name</span>
+          </label>
+          <input
+            className="input input-bordered input-primary w-full max-w-xs"
+            placeholder="99%"
+            id="name-input"
+            value={assignmentName}
+            onFocus={() => setSubError(undefined)}
+            onChange={(e) => setAssignmentName(e.target.value)}
+          />
+          {subError && <span>{subError}</span>}
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={onSubmitAssignment}
+          >
+            Submit Assignment
+          </button>
+        </Card>
         <Card title="Assignments" className="col-span-2 row-span-3"></Card>
       </div>
     </div>
