99ddb201b4c26417fab254c1b878e9a4b37320f4
James <jamesarnott2@gmail.com>
2023-06-15 14:45:41 +0100

    fix: throwing error if the frontend url is missing

--- packages/graphql/src/__generated__/resolvers-types.ts
+++ packages/graphql/src/__generated__/resolvers-types.ts
@@ -195,6 +195,8 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
+
+
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Assignments: ResolverTypeWrapper<Assignments>;
