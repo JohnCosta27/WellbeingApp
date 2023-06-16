e9af52ff459417be095a861be41304fce6854a16
James <jamesarnott2@gmail.com>
2023-06-16 14:00:35 +0100

    chore: react router update with file structure chage

--- apps/frontend/src/Router.tsx
+++ apps/frontend/src/Router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import { ProtectedRoute, Login, Register } from "./Auth";
 import { DashboardLayout } from "./DashboardLayout";
-import { HowDashboard } from "./HowDashboard";
+import { HowDashboard } from "./HowDashboard/";
 import { MyProgress } from "./MyProgress";
 import { WhoDashboard } from "./WhoDashboard";
 
