6b8f9fc3f8d557941365d5ff7ac0822208b43bfd
James <jamesarnott2@gmail.com>
2023-06-16 14:00:50 +0100

    feat: created a 'YourStats' card

---
+++ apps/frontend/src/HowDashboard/YourStats.tsx
@@ -0,0 +1,43 @@
+import { MentalEnergy } from "@wellbeing/graphql-types";
+import { getLast7DaysEnergy } from "../utils/misc";
+import React, { ReactNode } from "react";
+import { TbMathAvg } from 'react-icons/tb';
+import {HiOutlineEmojiHappy} from 'react-icons/hi';
+import {AiOutlineCalendar} from 'react-icons/ai';
+
+type YourStatsProps = {
+	sortedEnergy: MentalEnergy[];
+}
+
+type StatCardProps = {
+  children: ReactNode;
+	icon?: React.JSX.Element;
+}
+
+const StatCard = (props: StatCardProps) => (
+	<div className="badge-primary badge flex justify-center align-middle h-fit">
+		<div className="h-10 w-10 my-auto mx-3">
+			{props.icon}
+		</div>
+		{props.children}
+	</div>
+);
+
+export const YourStats = (props: YourStatsProps) => {
+	const {sortedEnergy} = props;
+
+	return (
+			<div className="grid gap-3 grid-flow-row">
+				<StatCard icon={<AiOutlineCalendar className="w-full h-full" />}>
+					Energy Levels Submitted Total: {sortedEnergy.length}.
+				</StatCard>
+				<StatCard icon={<HiOutlineEmojiHappy className="h-full w-full"/>}>
+					Good Days: {sortedEnergy.filter((e) => e.level > 0.5).length}.
+				</StatCard>
+				<StatCard icon={<TbMathAvg className="h-full w-full"/>}>
+					{/* Ceil > floor, to stay positive! */}
+					Average Last Week: {Math.ceil(getLast7DaysEnergy(sortedEnergy)*100)}%
+				</StatCard>
+			</div>
+	)
+};
