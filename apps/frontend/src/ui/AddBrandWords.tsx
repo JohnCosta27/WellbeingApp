5b3f9475fe4d3631d76db77710ce0df1f06c1635
James <jamesarnott2@gmail.com>
2023-06-18 11:41:44 +0100

    feat: updating brand words input to combo box combo box now works mostly need to send word(s) to the server

--- apps/frontend/src/ui/AddBrandWords.tsx
+++ apps/frontend/src/ui/AddBrandWords.tsx
@@ -4,6 +4,18 @@ import {
   useAddWholeBrandMutation,
 } from "@wellbeing/graphql-types";
 import { FC } from "react";
+import { Fragment, useState } from 'react'
+import { Combobox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+]
 
 interface AddBrandWordsProps {
   /** First element of tuple is the ID of the word */
@@ -17,6 +29,100 @@ interface AddBrandWordsProps {
  * TODO: turn this into a combo box https://headlessui.com/react/combobox
  */
 export const AddBrandWords: FC<AddBrandWordsProps> = ({
+  brandWords,
+  onAddWord,
+}) => {
+  const [addWholeBrandMutation] = useAddWholeBrandMutation({
+    refetchQueries: [namedOperations.Query.CurrentUser],
+  });
+
+  const [selected, setSelected] = useState(brandWords[0])
+  const [query, setQuery] = useState('')
+
+  const filteredBrands =
+    query === ''
+      ? brandWords
+      : brandWords.filter((w) =>
+          w.word
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        )
+
+  return (
+    <div className="w-72">
+      <Combobox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              displayValue={(w) => (w as BrandWords).word}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {brandWords.length === 0 && query !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredBrands.map((w) => (
+                  <Combobox.Option
+                    key={w.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                      }`
+                    }
+                    value={w}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {w.word}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  )
+};
+
+
+
+export const AddBrandWordsOld: FC<AddBrandWordsProps> = ({
   brandWords,
   onAddWord,
 }) => {
