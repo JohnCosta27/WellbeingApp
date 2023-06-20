1421f5085cf6d7cf5baf84ae1963e55659e06204
James <jamesarnott2@gmail.com>
2023-06-20 10:52:57 +0100

    refactor: moved isToday to own file

--- apps/frontend/src/utils/misc.ts
+++ apps/frontend/src/utils/misc.ts
@@ -4,3 +4,20 @@ export const getLast7DaysEnergy = (energies: MentalEnergy[]): number  => {
   if (energies.length === 0) return 0;
   return energies.reduce((p, n) => p + n.level, 0) / energies.length;
 };
+
+// Takes a date and returns if said date is today.
+export function isToday(givenDate: Date): boolean {
+  const today = new Date();
+  return (
+    today.getDate() === givenDate.getDate() &&
+    today.getMonth() === givenDate.getMonth() &&
+    today.getFullYear() === givenDate.getFullYear()
+  );
+}
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
+// Returns a timestmap number (in milliseconds), left until the next day.
+export function timeUntilEndOfDay(): number {
+  return ONE_DAY - (Math.floor(new Date().getTime()) % ONE_DAY);
+}
