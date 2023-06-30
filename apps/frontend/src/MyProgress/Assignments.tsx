fd54a0e2b29ff5734c9467509e78eb6016161659
James <jamesarnott2@gmail.com>
2023-06-30 15:05:38 +0100

    style: improved style of assignments, using daisyui cards

--- apps/frontend/src/MyProgress/Assignments.tsx
+++ apps/frontend/src/MyProgress/Assignments.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import { UserModules } from "@wellbeing/graphql-types";
 import { Card } from "../ui";
 
@@ -16,18 +17,30 @@ const Assignments = (props: AssignmentsProps) => {
     );
   }
 
+  if (modules.length === 0) {
+    return (
+      <Card title="Assignments" className="flex justify-center align-middle">
+        <div className="">You have no chosen modules yet.</div>
+      </Card>
+    );
+  }
+
   return (
-    <Card title="Assignments" className="col-span-2 row-span-3">
-      {modules.map((m) => (
-        <div key={m.module.id}>
-          <h2 className="text-lg">{m.module.name}</h2>
-          <ul>
-            {m.assignments.map((a) => (
-              <li key={a.date}>
-                {a.name} | {a.score}
-              </li>
-            ))}
-          </ul>
+    <Card className="col-span-2 row-span-3 gap-4 grid grid-cols-4 grid-rows-3">
+      {modules.map((m, i) => (
+        <div className="card bg-base-100 shadow-xl " key={i}>
+          <div className="card-title m-auto bg-info p-2 rounded-t-2xl">
+            {m.module.name}
+          </div>
+          <div className="card-body">
+            <ul>
+              {m.assignments.map((a) => (
+                <li key={a.date}>
+                  {a.name} | {a.score}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       ))}
     </Card>
