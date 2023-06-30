8878b4acd6bb41b52ba60c2a3d65926c7526a716
James <jamesarnott2@gmail.com>
2023-06-30 14:17:34 +0100

    chore: removed all of john's "eslint-ignore" comments lol

--- apps/frontend/src/MyProgress/index.tsx
+++ apps/frontend/src/MyProgress/index.tsx
@@ -1,9 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import {
   namedOperations,
   useAddAssignmentMutation,
