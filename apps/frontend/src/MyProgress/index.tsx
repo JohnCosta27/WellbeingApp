7e8c7befcd812d80e3c05bd629ec3eefb63564ba
James <jamesarnott2@gmail.com>
2023-07-05 19:07:58 +0100

    style: fixed mobile scaling

--- apps/frontend/src/MyProgress/index.tsx
+++ apps/frontend/src/MyProgress/index.tsx
@@ -15,7 +15,7 @@ export const MyProgress: FC = () => {
         <h2 className="text-3xl font-bold">My Progress</h2>
         <h4 className="text-xl">View how you are doing in your modules.</h4>
       </div>
-      <div className="w-full min-h-full grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-6">
+      <div className="w-full min-h-full flex flex-col lg:grid lg:grid-cols-3 gap-x-4 gap-y-6">
         <ModuleSelector user={user.data?.currentUser} />
         <SubmitAssignment modules={user.data?.currentUser.modules} />
         <OverallStats modules={user.data?.currentUser.modules} />
