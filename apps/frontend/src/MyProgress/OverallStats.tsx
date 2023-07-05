237c5c5bf18ea207c777b85efd347f71e019a2e5
James <jamesarnott2@gmail.com>
2023-07-05 15:38:22 +0100

    feat: created overall stats pie chart needs refactoring as it's half fp and not clear

--- apps/frontend/src/MyProgress/OverallStats.tsx
+++ apps/frontend/src/MyProgress/OverallStats.tsx
@@ -18,12 +18,93 @@ const data = {
   ],
 };
 
+type extractedData = {
+  modules: {
+    moduleName: string;
+    completedScore: number;
+    uncompletedAmount: number;
+  }[];
+  uncompletedAmount: number;
+};
+
 type OverallStatsProps = {
   modules: UserModules[] | undefined;
 };
 
 const OverallStats = (props: OverallStatsProps) => {
   const { modules } = props;
+
+  /**
+   * Extracts the data from the modules to be used in the chart
+   */
+  const extractData = () => {
+    const reduced = modules
+      ?.map((module) => {
+        const completedScore = module.assignments.reduce(
+          (acc, curr) => acc + (curr.score * curr.percent) / 100,
+          0
+        );
+
+        const uncompletedAmount =
+          100 - module.assignments.reduce((acc, curr) => acc + curr.percent, 0);
+
+        return {
+          moduleName: module.module.name,
+          completedScore,
+          uncompletedAmount,
+        };
+      })
+      .reduce(
+        (acc, curr) => {
+          acc.uncompletedAmount += curr.uncompletedAmount;
+          acc.modules.push(curr);
+          return acc;
+        },
+        { modules: [], uncompletedAmount: 0 } as extractedData
+      );
+
+    if (!reduced)
+      return {
+        labels: [],
+        datasets: [],
+      };
+    const chartLabels = reduced.modules.map((module) => module.moduleName);
+
+    const chartDataset = reduced.modules.reduce(
+      (acc, curr) => {
+        acc.data.push(curr.completedScore);
+        acc.backgroundColor.push(
+          `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
+        );
+        return acc;
+      },
+      { data: [], backgroundColor: [] } as {
+        data: number[];
+        backgroundColor: string[];
+      }
+    );
+
+    chartLabels.push("Uncompleted");
+    chartDataset.data.push(reduced.uncompletedAmount);
+    chartDataset.backgroundColor.push(
+      `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
+    );
+
+    const outVal = {
+      labels: chartLabels,
+      datasets: [
+        {
+          label: "Overall Year 1 Stats",
+          data: chartDataset.data,
+          backgroundColor: chartDataset.backgroundColor,
+          hoverOffset: 4,
+        },
+      ],
+    };
+
+    console.log(outVal);
+    return outVal;
+  };
 
   if (!modules) {
     return (
@@ -50,7 +131,7 @@ const OverallStats = (props: OverallStatsProps) => {
       <div className="card-title bg-info p-2 rounded-t-2xl w-full text-center flex justify-center align-middle h-16">
         <div className="m-auto text-2xl">Overall Stats</div>
       </div>
-      <DoughnutChart data={data} />
+      <DoughnutChart data={extractData()} />
     </div>
   );
 };
