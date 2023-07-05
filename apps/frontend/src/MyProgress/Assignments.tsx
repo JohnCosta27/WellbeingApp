dee3d923b725fb62c06fec16c5d7499a5140bf09
James <jamesarnott2@gmail.com>
2023-07-05 20:00:40 +0100

    style: updated assignments cards lots, they now look better

--- apps/frontend/src/MyProgress/Assignments.tsx
+++ apps/frontend/src/MyProgress/Assignments.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-array-index-key */
 import { UserModules } from "@wellbeing/graphql-types";
 import { Card } from "../ui";
+import CompletedBar from "./CompletedBar";
+import { reduceModules, scaleModuleOverallScore } from "../utils";
 
 type AssignmentsProps = {
   modules: UserModules[] | undefined;
@@ -29,27 +31,38 @@ const Assignments = (props: AssignmentsProps) => {
     <Card className="min-h-[50vh] col-span-2 row-span-2 gap-4 grid grid-cols-2 lg:grid-cols-4 grid-rows-3">
       {modules.map((m) => (
         <div className="card bg-base-100 shadow-xl " key={m.module.id}>
-          <div className="card-title bg-info p-2 rounded-t-2xl w-full text-center flex justify-center align-middle h-16">
-            <div className="m-auto text-base text-ellipsis overflow-hidden whitespace-pre">
+          <div className="card-title bg-info rounded-t-2xl w-full text-center flex flex-col justify-center align-middle h-16">
+            <div className="m-auto text-base text-ellipsis overflow-hidden whitespace-pre p-2">
               {m.module.name.split(":")[1].trim()}
             </div>
+            <CompletedBar
+              data={scaleModuleOverallScore(reduceModules([m]))}
+              className="h-6"
+            />
           </div>
-          <div className="card-body w-full p-0">
-            {m.assignments.length > 0 && (
-              <div className="w-full bg-info-content text-white p-2">
-                Average:{" "}
-                {m.assignments.reduce((a, b) => a + b.score, 0) /
-                  m.assignments.length}
-                %
-              </div>
-            )}
-            <ul className="p-2">
-              {m.assignments.map((a) => (
-                <li key={a.date}>
-                  {a.name} | {a.score} - {a.percent}% of module
-                </li>
-              ))}
-            </ul>
+          <div className="card-body w-full p-0 max-h-52 overflow-x-auto">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className=" rounded-t-none">Name</th>
+                  <th>Score</th>
+                  <th className=" rounded-t-none">% of Module</th>
+                </tr>
+              </thead>
+              <tbody className="overflow-x-auto">
+                {m.assignments.map((a) => (
+                  <tr key={a.date}>
+                    <td>
+                      {a.name.trim().length > 20
+                        ? `${a.name.trim().slice(0, 20)}...`
+                        : a.name.trim()}
+                    </td>
+                    <td>{a.score}</td>
+                    <td>{a.percent}%</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       ))}
