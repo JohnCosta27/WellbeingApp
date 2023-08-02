d5ded40f96ec21c25a20a8dfc44431a3ff43c40f
John Costa <johncosta027@gmail.com>
2023-08-02 14:36:39 +0100

    FEAT: Adding consent form

--- apps/frontend/src/Auth/Login.tsx
+++ apps/frontend/src/Auth/Login.tsx
@@ -72,6 +72,7 @@ export const Register: FC = () => {
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [checked, setChecked] = useState(0);
 
   const nav = useNavigate();
 
@@ -106,6 +107,53 @@ export const Register: FC = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center bg-base-100">
       <h1 className="mb-8 text-5xl">Professional Self Toolkit App</h1>
+      <div className="max-w-5xl flex flex-col gap-4 my-4">
+        <h2 className="text-2xl">PST Consent Statement</h2>
+        <p>
+          I understand that by using this Professional Self Toolkit App (‘the
+          App’) that I will be entering personal data about myself that will be
+          collected and stored for the purpose of providing me with the features
+          and functionality of this product and for no other purpose.
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={(e) => setChecked((c) => c + (e.target.checked ? 1 : -1))}
+          />
+        </p>
+
+        <p>
+          I consent to my personal data being collected and stored by the App
+          hosting company Microsoft Azure (server managed by John Costa) for the
+          purposes of providing me with the features, functionality and
+          operation of this product.
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={(e) => setChecked((c) => c + (e.target.checked ? 1 : -1))}
+          />
+        </p>
+
+        <p>
+          I understand that my personal data will not be shared with third
+          parties.
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={(e) => setChecked((c) => c + (e.target.checked ? 1 : -1))}
+          />
+        </p>
+        <p>
+          I understand that my use of this product is voluntary and that I can
+          withdraw my consent at any time and delete my data by selecting the
+          ‘delete data’ feature in this App without giving any notice or reason
+          for leaving.
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={(e) => setChecked((c) => c + (e.target.checked ? 1 : -1))}
+          />
+        </p>
+      </div>
       <div className="py-16 px-8 max-w-5xl w-96 flex flex-col gap-4 bg-white rounded-xl">
         <input
           type="email"
@@ -137,7 +185,17 @@ export const Register: FC = () => {
           placeholder="Confirm Password..."
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        <button type="submit" className="btn btn-primary" onClick={onLogin}>
+        {checked !== 4 && (
+          <p className="text-warning">
+            You must check all the consent boxes before registering.
+          </p>
+        )}
+        <button
+          disabled={checked !== 4}
+          type="submit"
+          className="btn btn-primary"
+          onClick={onLogin}
+        >
           Register
         </button>
       </div>
--- apps/frontend/src/Auth/index.ts
+++ apps/frontend/src/Auth/index.ts
@@ -1,7 +1,7 @@
-export * from './ProtectedRoute';
-export * from './Login';
-export * from './isTokenValid';
+export * from "./ProtectedRoute";
+export * from "./Login";
+export * from "./isTokenValid";
 
-export function setToken(type: 'access' | 'refresh', token: string) {
+export function setToken(type: "access" | "refresh", token: string) {
   localStorage.setItem(type, token);
 }
